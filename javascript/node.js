8154735567f68a27a09d2f7e6c62e126b7a7372d
Rodrigo Cavanha <15946186+rdcavanha@users.noreply.github.com>
2023-10-17 11:58:03 +0200

    feat(node): add javascript config

---
+++ javascript/node.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ['airbnb/base', '../prettier'],
+  rules: {
+    'import/prefer-default-export': 0,
+    'no-await-in-loop': 0,
+    'no-restricted-syntax': 0,
+  },
+};
