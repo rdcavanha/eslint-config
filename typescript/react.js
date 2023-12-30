580a38057ce701f5b6192a96783f89bef488da40
Rodrigo Cavanha <15946186+rdcavanha@users.noreply.github.com>
2023-12-30 00:56:12 -0300

    feat(react-ts): add airbnb/hooks

--- typescript/react.js
+++ typescript/react.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ['airbnb', 'airbnb-typescript', '../prettier'],
+  extends: ['airbnb', 'airbnb-typescript', 'airbnb/hooks', '../prettier'],
   rules: {
     'react/function-component-definition': 0,
     'react/react-in-jsx-scope': 0,
